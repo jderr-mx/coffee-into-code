aeee4cd7a6add6f7fd168b851fc4a6ff6ad66009
John Derr <john.derr@maximos.io>
2020-07-22 22:04:40 -0400

    trying to get the location right

--- app/routes/post.js
+++ app/routes/post.js
@@ -32,7 +32,7 @@ export default class PostRoute extends Route{
       },
       {
         name: 'twitter:image',
-        content: `${document.location.host}${post.image}`
+        content: `${location.protocol}//${document.location.host}${post.image}`
       },
       {
         name: 'twitter:image:alt',
