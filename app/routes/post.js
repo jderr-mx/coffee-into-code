2dd202280e07e0ff6e9d3da33a119c5e31d95666
John Derr <john.derr@maximos.io>
2020-07-22 21:28:02 -0400

    added meta tag info for twitter cards, TODO probably refactor to the theme

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,8 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import { hash } from 'rsvp';
 
-export default Route.extend({
-  classNames: ["post-template"],
+export default class PostRoute extends Route{
+  @service headData;
+  classNames =  ["post-template"];
 
   model(params) {
     // load authors first for ember-data autopopulation
@@ -12,54 +14,31 @@ export default Route.extend({
         posts: this.store.findAll('content'),
       });
     });
-  },
+  }
 
   afterModel({post}) {
-
     const headTags = [
       {
-        type: 'meta',
-        tagId: 'twitter-card',
-        attrs: {
-          name: 'twitter:card',
-          content: 'summary_large_image'
-        }
+        name: 'twitter:card',
+        content: 'summary_large_image'
       },
       {
-        type: 'meta',
-        tagId: 'twitter-site',
-        attrs: {
-          name: 'twitter:site',
-          content: '@johnderrdotnet'
-        }
+        name: 'twitter:site',
+        content: '@johnderrdotnet'
       },
       {
-        type: 'meta',
-        tagId: 'twitter-title',
-        attrs: {
-          name: 'twitter:title',
-          content: post.title
-        }
+        name: 'twitter:title',
+        content: post.title
       },
       {
-        type: 'meta',
-        tagId: 'twitter-image',
-        attrs: {
-          name: 'twitter:image',
-          content: post.image
-        }
+        name: 'twitter:image',
+        content: post.image
       },
       {
-        type: 'meta',
-        tagId: 'twitter-image-alt',
-        attrs: {
-          name: 'twitter:image:alt',
-          content: post.title
-        }
+        name: 'twitter:image:alt',
+        content: post.title
       },
-
-
     ];
-    this.set('headTags', headTags);
+    this.headData.headTags = headTags;
   }
-});
+}
