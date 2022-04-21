cdeac29c1b6af0bd8f7598eaa540c5e0d3d41dd3
John Derr <john.derr@allovue.com>
2022-04-20 22:30:12 -0400

    disable uglify?

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'ember-cli-uglify': {
+      uglify: {
+        compress: false
+      },
+    },
     fingerprint: {
       extensions: ['js', 'css', 'map']
     },
